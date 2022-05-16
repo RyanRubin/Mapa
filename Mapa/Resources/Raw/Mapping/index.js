66caec7c03529a0d6d3843bc174f1ba19624a502
Ryan Rubin <ryan.rubin@ph.softvision.global>
2022-05-16 23:42:05 +0800

    add icon functionality

--- Mapa/Resources/Raw/Mapping/index.js
+++ Mapa/Resources/Raw/Mapping/index.js
@@ -1,29 +1,66 @@
 class App {
+    map;
+    tileLayer;
+    bingMapsSource;
+    offlineMapsSource;
+    vectorLayer;
+    vectorSource;
+
     initMap() {
-        const bingMapsSource = new ol.source.BingMaps({
+        // window.addEventListener('contextmenu', e => e.preventDefault());
+
+        this.bingMapsSource = new ol.source.BingMaps({
             key: 'AkGBls_Q10K9PW0jYdc1KuSPiF5qOXXICG3D9F2cT5QWLzdJsVYwgl8JYpnZg8sE',
             imagerySet: 'Aerial',
+            maxZoom: 19,
+            tileLoadFunction: (imageTile, src) => {
+                imageTile.getImage().src = src;
+                // console.log(imageTile.tileCoord);
+            }
+        });
+
+        this.offlineMapsSource = new ol.source.OSM({
             tileLoadFunction: (imageTile, src) => {
                 imageTile.getImage().src = src;
             }
         });
 
-        const offlineMapsSource = new ol.source.OSM({
-            tileLoadFunction: (imageTile, src) => {
-                imageTile.getImage().src = src;
-            }
+        this.tileLayer = new ol.layer.Tile({
+            source: this.bingMapsSource
         });
 
-        new ol.Map({
+        this.vectorSource = new ol.source.Vector();
+
+        this.vectorLayer = new ol.layer.Vector({
+            source: this.vectorSource
+        });
+
+        this.map = new ol.Map({
             target: 'map',
-            layers: [new ol.layer.Tile({
-                source: bingMapsSource
-            })],
+            layers: [this.tileLayer, this.vectorLayer],
             view: new ol.View({
                 center: ol.proj.fromLonLat([-81.49559810202766, 41.469741498180625]),
                 zoom: 18
             })
         });
+
+        this.addIcon(this.map.getView().getCenter());
+    }
+
+    addIcon(coord) {
+        const iconFeature = new ol.Feature({
+            geometry: new ol.geom.Point(coord)
+        });
+        iconFeature.setStyle(new ol.style.Style({
+            image: new ol.style.Icon({
+                anchor: [0.5, 46],
+                anchorXUnits: 'fraction',
+                anchorYUnits: 'pixels',
+                src: 'img/icon.png'
+            })
+        }));
+
+        this.vectorSource.addFeature(iconFeature);
     }
 }
 
